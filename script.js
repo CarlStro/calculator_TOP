280a8612c4fda92cb1b94e61e9a7c38cb1d3c52a
CarlStro <121464561+CarlStro@users.noreply.github.com>
2023-05-15 10:58:38 -0500

    Add basic operations for calculator in JS

---
+++ script.js
@@ -0,0 +1,32 @@
+function add(a, b) {
+    return a + b;
+}
+
+function subtract(a, b) {
+    return a - b;
+}
+
+function multiply(a, b) {
+    return a * b;
+}
+
+function divide(a, b) {
+    return a / b;
+}
+
+let firstNum = 0;
+let secondNum = 0;
+let operator = null;
+
+function operate(a, b, operator) {
+    switch (operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            return divide(a, b);
+    }
+}
